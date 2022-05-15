940835ad5dddf850061151557699c45729a69700
Saroj Rai <raisaroj360@gmail.com>
2022-05-15 07:24:39 +0545

    working undeterministically

--- index.js
+++ index.js
@@ -1,66 +1,21 @@
-const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
-const { elementsLocated, elementIsEnabled } = require('selenium-webdriver/lib/until');
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
 
-(async function getVotesForKathmandu() {
-  let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
-  let datas = []; 
-  try {
-    await driver.get('https://localelection.ekantipur.com/pradesh-3/district-kathmandu?lng=eng');
-    await driver.sleep(1000);
+const { getVotesForKathmandu } = require('./scrapper.js');
 
-    const iframeElements = await driver.wait(until.elementsLocated(By.css('iframe')), 10000);
-    if(iframeElements.length){
-      await driver.executeScript(`
-      let elems = document.getElementsByTagName("iframe"); 
-      for(let i = elems.length-1; i >= 0; i--) {
-        elems[i].remove();
-      }
-      elems = document.getElementsByTagName("ins"); 
-      for(let i = elems.length-1; i >= 0; i--) {
-        elems[i].remove();
-      }      
-      `);
-    }
 
-    const elements = await driver.wait(until.elementsLocated(By.className('accordion-title')), 10000);
-    const size = elements.length;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-    for(let i = 0; i < size; i++){
-      await driver.executeScript('arguments[0].scrollIntoView(true);', elements[i]);
-      await driver.sleep(500);
-      await driver.executeScript('window.scrollBy(0, -window.innerHeight / 4);');      
-      await driver.sleep(500);      
-      elements[i].click();
-      
-      let parent = await driver.executeScript('return arguments[0].parentNode;', elements[i]);
-      let grandParent = await driver.executeScript('return arguments[0].parentNode;', parent);      
-      let accordionElement = await driver.executeScript('return arguments[0].parentNode', grandParent);
+app.get('/api/v1/kathmandu', async (req, res) => {
+  res.json(await getVotesForKathmandu());
+});
 
-      // wait until elementsLocated with className 'candidate-name'      
-      const candidateNames = await driver.wait(until.elementsLocated(By.className('candidate-name')), 10000);
-      const voteNumbers = await driver.wait(until.elementsLocated(By.className('vote-numbers')), 10000);
-
-      datas.push({
-        'name': await parent.getText(),
-        'candidateNames': await Promise.all(candidateNames.map(async (candidateName) => {
-          return await candidateName.getText();
-        })),
-        'voteNumbers': await Promise.all(voteNumbers.map(async (voteNumber) => { 
-          return await voteNumber.getText();
-        }))
-      });
-
-      // delete accordion element
-      await driver.executeScript('arguments[0].remove();', accordionElement);
-      await driver.sleep(100);
-    }
-  
-    console.log(datas);
-    
-    return datas;
-  } finally {
-    await driver.quit();
-  }
-})();
+app.listen(port, () => {
+  console.log(`Server app listening on port ${port}`);
+});
 
 
---
+++ scrapper.js
@@ -0,0 +1,77 @@
+const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
+const { elementsLocated, elementIsEnabled } = require('selenium-webdriver/lib/until');
+
+async function getVotesForKathmandu() {
+  let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+  let datas = []; 
+  try {
+    await driver.get('https://localelection.ekantipur.com/pradesh-3/district-kathmandu?lng=eng');
+    await driver.sleep(1000);
+
+    const iframeElements = await driver.wait(until.elementsLocated(By.css('iframe')), 10000);
+    if(iframeElements.length){
+      await driver.executeScript(`
+      let elems = document.getElementsByTagName("iframe"); 
+      for(let i = elems.length-1; i >= 0; i--) {
+        elems[i].remove();
+      }
+      elems = document.getElementsByTagName("ins"); 
+      for(let i = elems.length-1; i >= 0; i--) {
+        elems[i].remove();
+      }      
+      `);
+    }
+
+    const elements = await driver.wait(until.elementsLocated(By.className('accordion-title')), 10000);
+    const size = elements.length;
+
+    for(let i = 1; i < 2; i++){
+      await driver.executeScript('arguments[0].scrollIntoView(true);', elements[i]);
+      await driver.sleep(500);
+      await driver.executeScript('window.scrollBy(0, -window.innerHeight / 4);');      
+      await driver.sleep(500);      
+      elements[i].click();
+
+      await driver.sleep(500);
+      
+      let parent = await driver.executeScript('return arguments[0].parentNode;', elements[i]);
+      let grandParent = await driver.executeScript('return arguments[0].parentNode;', parent);      
+      let accordionElement = await driver.executeScript('return arguments[0].parentNode', grandParent);
+
+      // wait until elementsLocated with className 'candidate-name'      
+      const candidateNames = await driver.wait(until.elementsLocated(By.className('candidate-name')), 10000);
+      const voteNumbers = await driver.wait(until.elementsLocated(By.className('vote-numbers')), 10000);
+      const candidateImages = await driver.wait(until.elementsLocated(By.className('candidate-img')), 10000);
+
+      datas.push({
+        'name': await parent.getText(),
+        'candidateNames': await Promise.all(candidateNames.map(async (candidateName) => {
+          return await candidateName.getText();
+        })),
+        'voteNumbers': await Promise.all(voteNumbers.map(async (voteNumber) => { 
+          return await voteNumber.getText();
+        })), 
+        candidateImages: await Promise.all(candidateImages.map(async (candidateImage) => {
+          // get child of candidateImage
+          let child = await driver.executeScript('return arguments[0].childNodes[0];', candidateImage);
+          return await child.getAttribute('src');
+        })),
+      });
+
+      // delete accordion element
+      await driver.executeScript('arguments[0].remove();', accordionElement);
+      await driver.sleep(100);
+    }
+  
+    console.log(datas);
+    
+    return datas;
+  } catch (e) { 
+    console.log(e.message);
+    return [];
+  } finally {
+    await driver.quit();
+  }
+}
+
+module.exports = { getVotesForKathmandu };
